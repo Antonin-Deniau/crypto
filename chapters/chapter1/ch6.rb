8a53d93eeb1798b5415330d3365af8fd38a3b0fa
DENIAU Antonin <antonin.deniau@gmail.com>
2016-08-29 17:58:01 +0200

    hammer

--- chapters/chapter1/ch6.rb
+++ chapters/chapter1/ch6.rb
@@ -2,60 +2,32 @@
 require 'base64'
 #extend RSpec::Matchers
 
-def find_string text
-  size = text.size
-  # Build a map from each (1-character) string to a list of its positions
-  roots = {}
-  size.times do |o|
-    s = text[o,1]
-    if roots.has_key? s
-      roots[s] << o
-    else
-      roots[s] = [o]
-    end
-  end
-
-  len = 1
-  first = nil
-
-  while true do
-    # Remove entries which don't have at least 2 non-overlapping  
-    roots.delete_if do |s,offsets|
-      count = 0
-      last = nil
-      offsets.each do |o|
-        next if last && last+len > o
-        last = o
-        count += 1
-      end
-      count < 2
-    end
-    break if roots.size == 0
-    first = roots[roots.keys[0]][0]
-
-    # Increase len by 1 and replace each existing root with the set of  
-    len += 1
-    new_roots = {}
-    roots.each do |s,offsets|
-      offsets.each do |o|
-        next if o > size - len
-        s = text[o,len]
-        if new_roots.has_key? s
-          new_roots[s] << o
-        else
-          new_roots[s] = [o]
-        end
-      end
-    end
-    roots = new_roots
-  end
-
-  [text[first,len-1], first, len-1]
-end
-
 text = File.open("#{File.dirname(__FILE__)}/ch6.txt").read.each_byte.map { |i| i }
 
-puts find_string(text).inspect
+def hamming text_1, text_2
+  def hamming_byte a, b
+    a = a.to_s(2)
+    b = b.to_s(2)
+
+    differences = 0
+
+    [a.length, b.length].min.times.count do |index|
+      differences += 1 if a[index] != b[index]
+    end
+  end
+
+  puts text_1.bytes.map { |i| i.to_s(2).rjust(8, ' ') }.join("-")
+  puts text_2.bytes.map { |i| i.to_s(2).rjust(8, ' ') }.join("-")
+
+  text_1 = text_1.bytes
+  text_2 = text_2.bytes
+
+  out = text_1.zip text_2
+
+  out.map { |i| hamming_byte(i[0], i[1]) }.inject(0, :+)
+end
+
+puts hamming("this is a test", "wokka wokka!!!")
 
 =begin
 ################################################
