3e71787b9954d6e5237c42d6701bdd64327ba4c3
DENIAU Antonin <antonin.deniau@gmail.com>
2016-08-31 13:11:40 +0200

    ch11

--- chapters/set2/ch11.rb
+++ chapters/set2/ch11.rb
@@ -1,31 +1,56 @@
-=begin
-Now that you have ECB and CBC working:
+require_relative '../../utils'
+require 'openssl'
+require 'base64'
 
-Write a function to generate a random AES key; that's just 16 random bytes.
+def ecb(key, text)
+  crypto = OpenSSL::Cipher.new('AES-128-ECB')
+  crypto.key = key
 
-Write a function that encrypts data under an unknown key --- that is, a function that generates a random key and encrypts under it.
-
-The function should look like:
-
-encryption_oracle(your-input)
-=> [MEANINGLESS JIBBER JABBER]
-Under the hood, have the function append 5-10 bytes (count chosen randomly) before the plaintext and 5-10 bytes after the plaintext.
-
-Now, have the function choose to encrypt under ECB 1/2 the time, and under CBC the other half (just use random IVs each time for CBC). Use rand(2) to decide which to use.
-
-Detect the block cipher mode the function is using each time. You should end up with a piece of code that, pointed at a block box that might be encrypting ECB or CBC, tells you which one is happening.
-=end
-
-def generate_aes_key
+  crypto.update(text) + crypto.final
 end
 
-def generate_data
+def cbc(key, text)
+  iv =  (rand(0..255)).chr * key.length
+
+  res = ""
+
+  text.bytes.each_slice(16) do |slice|
+    slice = slice.pack("C*")
+
+    crypto = OpenSSL::Cipher.new('AES-128-ECB')
+    crypto.key = key
+    crypto.padding = 0
+
+    out = crypto.update(slice) + crypto.final
+
+    res << Utils.encode_xor(iv, out.bytes).pack("C*")
+
+    iv = slice
+  end
+  res
 end
 
-def append_bytes
+def generate_key
+  (0..15).to_a.map { |i| rand(0..255) }.pack("C*")
 end
 
-def encrypt_ecb_cbc
+def generate_random
+  (0..rand(4..9)).to_a.map { |i| rand(0..255) }.pack("C*")
+end
+
+def generate_data(data)
+  data = generate_random + data + generate_random
+  data = data + ("\x00" * (16 - (data.length % 16)))
+
+  alg = rand(2) == 1 ? 'ECB' : 'CBC'
+  key = generate_key
+
+  if alg == 'CBC'
+    cbc key, data
+  else
+    ecb key, data
+  end
 end
 
 
+puts generate_data("1" * 16)
